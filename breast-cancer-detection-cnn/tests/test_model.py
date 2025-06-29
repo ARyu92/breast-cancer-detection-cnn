3cc89a7b8d0499549ee81037d13bc7f41c553495
Aryu92 <alexryu29@gmail.com>
2025-06-29 17:37:17 +0200

    Initial model class implementation and unit testing

--- breast-cancer-detection-cnn/tests/test_model.py
+++ breast-cancer-detection-cnn/tests/test_model.py
@@ -1,8 +1,8 @@
 import unittest
 import sys
 import os
-import pydicom
 import numpy as np
+from tensorflow import keras
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
@@ -20,7 +20,38 @@ class test_image_processor(unittest.TestCase):
     #Tests that this class object exists.
     def test_class_exist(self):
         self.assertIsInstance(self.model, Model)
-        return
+        
+    
+    #Tests that the network has the specified number of layers.
+    def test_class_exist(self):
+        #List holds each layer's parameters in a dictionary object.
+        layer_input = [
+            {"layer_type": "Conv2D", 
+             "filters" : 32,
+             "kernel" : (3,3), 
+              "activation": "relu" },
+            {"layer_type": "Conv2D", 
+             "filters" : 64,
+             "kernel" : (3,3), 
+              "activation": "relu" },
+            {"layer_type": "Conv2D", 
+             "filters" : 128,
+             "kernel" : (3,3), 
+              "activation": "relu" },
+            {"layer_type": "Flatten"},
+            {"layer_type": "Dense", 
+             "units" : 128,
+              "activation": "relu" },
+            {"layer_type": "Dense", 
+             "units" : 128,
+              "activation": "softmax" }
+              
+        ]
+        self.model.build_network(layer_input)
+        num_of_layers = len(self.model.neural_network.layers)
+
+        self.assertEqual(num_of_layers, 9)
+
 
 if __name__ == "__main__":
     unittest.main()
