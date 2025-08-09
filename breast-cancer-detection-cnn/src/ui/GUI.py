455109b880e15eac2869514fb65aae385f26c6a9
Aryu92 <alexryu29@gmail.com>
2025-08-09 19:46:19 +0200

    UI wireframe re-write

--- breast-cancer-detection-cnn/src/ui/GUI.py
+++ breast-cancer-detection-cnn/src/ui/GUI.py
@@ -4,62 +4,73 @@ import numpy as np
 class GUI:
     def __init__(self):
         st.set_page_config(layout="wide")
-        #Header
-        #st.title("Breast Cancer Detection")
-        #st.markdown("An AI Powered Application")
+        # Reduce top and bottom padding
+        st.markdown(
+            """
+            <style>
+            .block-container {
+                padding-top: 0rem;
+                padding-bottom: 1rem;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
 
-        #Header
-        head_pad1, head_col1, head_pad2,  = st.columns([1.5, 1, 7.5])
+#--------------------------------------------------------------Header--------------------------------------------------------#
+        header = st.container()
+        with header:
+            st.markdown("## Breast Cancer Detection")
+            st.markdown("An AI Powered Application")
 
-        with head_col1:
-            st.button("Load Image", use_container_width= True)
 
-        #Main body
-        placeholder_img = np.zeros((400, 400, 3), dtype=np.uint8)
+#--------------------------------------------------------------Navigator-----------------------------------------------------#
+        tool_pad1, tool_col1, tool_col2, tool_pad2,  = st.columns([1.5, 1, 1, 6.5])
 
-        col1, col2, col3, col_pad1 = st.columns([1.5,4,4,0.5])
+        with tool_col1:
+            st.button("Load Image", use_container_width= True, key = "load_immage_button")
 
-        with col1: 
+        with tool_col2:
+            st.button("Load Model", use_container_width= True, key = "load_model_button")
+
+#--------------------------------------------------------------Main Body-----------------------------------------------------#
+        info_bar, cc_image_holder, body_pad1 ,mlo_image_holder, body_pad2 = st.columns([1.5,3.5, .2, 3.5, 0.8])
+
+        with info_bar: 
             st.subheader("Patient:")
             name_box = st.empty()
-            name = "Dave"
-
+            name = "example"
             
             name_box.write(f"Patient Name: {name}")
 
-        with col2:
-            #st.subheader("CC Image")
-            st.image(placeholder_img, caption="Craniocaudal", use_container_width =True)
+        placeholder_img = np.zeros((400, 400, 3), dtype=np.uint8)
+        with cc_image_holder:
+            cc_image_slot = st.empty()
+            cc_image_slot.image(image = placeholder_img, caption="Craniocaudal", use_container_width =True)
 
-        with col3:
-            #st.subheader("MLO Image")
-            st.image(placeholder_img, caption= "Mediolateral Oblique", use_container_width =True)
+        with mlo_image_holder:
+            mlo_image_slot = st.empty()
+            mlo_image_slot.image(image = placeholder_img, caption= "Mediolateral Oblique", use_container_width =True)
 
-        
+#--------------------------------------------------------------Input_output-------------------------------------------------#
+        model_options, console, malignant_marker, bengin_marker, foot_pad1 = st.columns([1.5, 6.5, 0.75 ,0.75, 0.5])
 
-
-        #Footer 
-        foot_col1, foot_col2, foot_col3, foot_col4, foot_pad1 = st.columns([1.5, 6.5, 0.75 ,0.75, 0.5])
-
-        with foot_col1:
-            st.write("Model")
+        with model_options:
+            st.write("Model Options")
             st.button("⚙️")
 
-        with foot_col2:
-            output_text = ""
+        with console:
+            output_text = "sample console output"
             st.write("Console")
-            st.markdown(f"""
-                <div style='background-color: white; padding: 20px; height: 100px; border-radius: 8px;
-                            box-shadow: 0 0 5px rgba(20,20,20,0.1); overflow-y: auto; white-space: pre-line;'>
-                    <p>{output_text}</p>
-                </div>
-            """, unsafe_allow_html=True)
 
-        with foot_col3:
-            st.markdown("<br>", unsafe_allow_html= True)
-            st.button("Malignant", key = "malignant")
+            st.code(output_text)
 
-        with foot_col4:
-            st.markdown("<br>", unsafe_allow_html= True)
-            st.button("Benign", key = "benign")
+        with malignant_marker:
+            st.markdown("")
+            st.markdown("")
+            st.button("Malignant", use_container_width= True, key = "malignant_marker_button")
 
+        with bengin_marker:
+            st.markdown("")
+            st.markdown("")
+            st.button("Benign", use_container_width= True, key = "benign_marker_button")
